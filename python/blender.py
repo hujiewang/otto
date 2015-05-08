b17eb8722176b9cb1f62db4e15626aa1cb0cdb37
Hujie Wang <wanghujie@gmail.com>
2015-05-08 12:05:54 -0400

    Added a nice blender

---
+++ python/blender.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+import os
+import random
+# Load validation data set
+
+valid = pd.read_csv("./data/valid_data.csv")
+
+labels = valid['target']
+
+class2num={"Class_1":1,"Class_2":2,"Class_3":3,"Class_4":4,"Class_5":5,"Class_6":6,"Class_7":7,"Class_8":8,"Class_9":9}
+for k,v in class2num.items():
+    tmp=[0]*9
+    for i in range(9):
+        if i+1 == v:
+            tmp[i]=1.0
+    class2num[k]=tmp
+
+valid_y=[]
+for i in range(0,len(labels.values)):
+    valid_y.append(class2num[labels.values[i]])
+valid_y=np.array(valid_y)
+
+
+# Load list of model results
+predictions = []
+results = []
+fname=[
+       "./valid_results/valid_6.csv",
+       "./valid_results/valid_8.csv",
+       "./valid_results/valid_11.csv",
+       "./valid_results/valid_13.csv",
+       "./valid_results/valid_14.csv",
+       "./valid_results/valid_15.csv"
+       ]
+fname_rv=[
+       "./results/results_6.csv",
+       "./results/results_8.csv",
+       "./results/results_11.csv",
+       "./results/results_13.csv",
+       "./results/results_14.csv",
+       "./results/results_15.csv"
+       ]
+
+for f in fname:
+    data=pd.read_csv(f)
+    _data=data[["Class_1","Class_2","Class_3","Class_4","Class_5","Class_6","Class_7","Class_8","Class_9"]]
+    predictions.append(_data.values)
+
+# finding the optimum weights
+
+def log_loss_func(weights):
+    ''' scipy minimize will pass the weights as a numpy array '''
+    final_prediction = 0
+    for weight, prediction in zip(weights, predictions):
+            final_prediction += weight*prediction
+
+    return log_loss(valid_y, final_prediction)
+
+best_score= 100.0
+for i in range(2):
+
+    weights = [0.0]*len(predictions)
+    for i in range(len(weights)):
+        weights[i]=random.uniform(0, 1)
+
+    cons = ({'type':'eq','fun':lambda w: 1-sum(w)})
+    bounds = [(0,1)]*len(predictions)
+
+    res = minimize(log_loss_func, weights, method='SLSQP', bounds=bounds, constraints=cons)
+
+    print('Ensamble Score: {best_score}'.format(best_score=res['fun']))
+    print('Weights: {weights}'.format(weights=res['x']))
+    if res['fun']<best_score:
+        best_score = res['fun']
+        best_weights=res['x']
+    print('Best Ensamble Score: {best_score}'.format(best_score=best_score))
+
+
+print('Blending models...')
+
+
+for f in fname_rv:
+    data=pd.read_csv(f)
+    _data=data[["Class_1","Class_2","Class_3","Class_4","Class_5","Class_6","Class_7","Class_8","Class_9"]]
+    results.append(_data.values)
+
+
+final_rv = 0
+for weight,result in zip(best_weights, results):
+    final_rv += weight*result
+
+
+df = pd.DataFrame(final_rv) # A is a numpy 2d array
+df.index+=1
+headers = ["Class_1","Class_2","Class_3","Class_4","Class_5","Class_6","Class_7","Class_8","Class_9"]
+df.to_csv("A.csv", header=headers,index=True, index_label = 'id') # C is a list of string corresponding to the title of each column of A
+
+print('Blended!')
